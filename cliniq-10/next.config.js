4001c623a93e79fbe0f050806d1ac5a815f0b6ab
viniciusmfrazao <viniciusmfrazao@gmail.com>
2026-04-10 00:10:12 -0300

    chore: limpa arquivos duplicados

--- cliniq-10/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['@cliniq/ui', '@cliniq/types', '@cliniq/utils'],
-  images: {
-    remotePatterns: [{ protocol: 'https', hostname: '*.supabase.co' }],
-  },
-}
-module.exports = nextConfig
